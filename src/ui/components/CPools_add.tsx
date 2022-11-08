2fa99e9859e83b5f783141aff90bae6f8c127dc3
robert <191335538@qq.com>
2022-11-08 21:55:26 +0800

    push

--- src/ui/components/CPools_add.tsx
+++ src/ui/components/CPools_add.tsx
@@ -3,9 +3,9 @@ import { useEffect, useState } from "react";
 import { ChainId, ethereum, web3 } from "../../app/Config";
 import { useLocation } from "react-router-dom";
 import { Network, Alchemy } from 'alchemy-sdk';
-import { getErc20Contract,getErc721Contract,getUniswapV3Router,getTokenB } from "../../app/Contract";
-import { Progress,message } from 'antd';
-import {  Modal,Radio  } from 'antd';
+import { getErc20Contract, getErc721Contract, getUniswapV3Router, getTokenB } from "../../app/Contract";
+import { Progress, message } from 'antd';
+import { Modal, Radio } from 'antd';
 import type { RadioChangeEvent } from 'antd';
 
 import {
@@ -28,42 +28,43 @@ import { url } from "inspector";
 interface PoolInfo {
    poolsAddress: any,
    nft_address: any,
-   id:any,
-   tokenA:any,
-   tokenB:any,
-   fractionNFTAddress:any
- }
+   id: any,
+   tokenA: any,
+   tokenB: any,
+   fractionNFTAddress: any
+}
 
 
- interface PoolOrder {
+interface PoolOrder {
    nft_address: any,
-   tokenB:any,
-   tokenId:any,
-   _amountA:any,
-   _amountB:any,
-   nftApprove:false,
-   tokenBApprove:any,
-   tokenAApprove:any
- }
+   tokenB: any,
+   tokenId: any,
+   _amountA: any,
+   _amountB: any,
+   nftApprove: false,
+   tokenBApprove: any,
+   tokenAApprove: any
+}
 
 
 export function CPools_add() {
    const dispatch = useAppDispatch();
    const { state } = useLocation();
    let nftaddress = state.address;
-   const [poolInfo,setPoolInfo] = useState({} as PoolInfo);
-   const [poolOrder,setPoolOrder] = useState({} as PoolOrder);
-   
+   console.log("nftaddress==================>" + nftaddress);
+   const [poolInfo, setPoolInfo] = useState({} as PoolInfo);
+   const [poolOrder, setPoolOrder] = useState({} as PoolOrder);
+
 
    const [isModalOpen, setModalOpen] = useState(false);
    const { address, chainId } = useAppSelector(ConnectSelectors.userData);
    const [myNfts, setMyNfts] = useState([] as Array<any>);
-    
 
-  
 
-   async function getNftsForOwner(){
-      if(!address){
+
+
+   async function getNftsForOwner() {
+      if (!address) {
          return;
       }
       const settings = {
@@ -71,150 +72,187 @@ export function CPools_add() {
          network: Network.MATIC_MUMBAI,
       };
       const alchemy = new Alchemy(settings);
-	   let 	e = await alchemy.nft.getNftsForOwner(address);
+      let e = await alchemy.nft.getNftsForOwner(address);
       let newArr = [] as Array<any>;
-         for (let index = 0; index < e.ownedNfts.length; index++) {
-            let item = e.ownedNfts[index];
-            if(item.contract.address == nftaddress.toLowerCase()){
-               let data: { name: any, tokenUrl: any, tokenId: any } = { name: "", tokenUrl: '', tokenId: "" };
-               data.name = item.contract.name;
-               data.tokenUrl = item.tokenUri ? item.tokenUri.gateway : '';
-               data.tokenId = item.tokenId
-               newArr.push(data);
-            }
+      for (let index = 0; index < e.ownedNfts.length; index++) {
+         let item = e.ownedNfts[index];
+         if (item.contract.address == nftaddress.toLowerCase()) {
+            let data: { name: any, tokenUrl: any, tokenId: any } = { name: "", tokenUrl: '', tokenId: "" };
+            data.name = item.contract.name;
+            data.tokenUrl = item.tokenUri ? item.tokenUri.gateway : '';
+            data.tokenId = item.tokenId
+            newArr.push(data);
          }
-         setMyNfts(newArr);
+      }
+      setMyNfts(newArr);
    }
 
 
    async function initSwap() {
       const uniswapV3Router = await getUniswapV3Router();
-      let  info =await uniswapV3Router.methods.getPoolInfo(nftaddress,"0x56223BAe67e6B26E6d1FC8B10431536235eD5B18",0).call();
+      let info = await uniswapV3Router.methods.getPoolInfo(nftaddress, "0x56223BAe67e6B26E6d1FC8B10431536235eD5B18", 0).call();
       console.log(info);
-      if(info.length > 0 ){
-         poolInfo.poolsAddress=info[0];
-         poolInfo.nft_address=info[1];
-         poolInfo.id=info[2];
-         poolInfo.tokenA=info[3];
-         poolInfo.tokenB=info[4];
-         poolInfo.fractionNFTAddress=info[5];
+      if (info.length > 0) {
+         poolInfo.poolsAddress = info[0];
+         poolInfo.nft_address = info[1];
+         poolInfo.id = info[2];
+         poolInfo.tokenA = info[3];
+         poolInfo.tokenB = info[4];
+         poolInfo.fractionNFTAddress = info[5];
          setPoolInfo(poolInfo);
          getErc20Approve();
          getErc721Approve();
-        
+
       }
    }
 
-   async function getErc20Approve(){
+   async function getErc20Approve() {
       const tokenBContract = await getErc20Contract(poolInfo.tokenB);
-      poolOrder.tokenBApprove=await tokenBContract.methods.allowance(address,poolInfo.poolsAddress).call();
+      poolOrder.tokenBApprove = await tokenBContract.methods.allowance(address, poolInfo.poolsAddress).call();
       const tokenAContract = await getErc20Contract(poolInfo.tokenA);
-      poolOrder.tokenAApprove=await tokenAContract.methods.allowance(address,poolInfo.poolsAddress).call();
-      setPoolOrder({...poolOrder});
-    }
+      poolOrder.tokenAApprove = await tokenAContract.methods.allowance(address, poolInfo.poolsAddress).call();
+      setPoolOrder({ ...poolOrder });
+   }
 
-    async function getErc721Approve(){
-      const contract= await getErc721Contract(poolInfo.nft_address);
-      poolOrder.nftApprove=await contract.methods.isApprovedForAll(address,poolInfo.fractionNFTAddress).call();
-      setPoolOrder({...poolOrder});
-    }
+   async function getErc721Approve() {
+      const contract = await getErc721Contract(poolInfo.nft_address);
+      poolOrder.nftApprove = await contract.methods.isApprovedForAll(address, poolInfo.fractionNFTAddress).call();
+      setPoolOrder({ ...poolOrder });
+   }
 
-    async function initOrder(){
+   async function initOrder() {
+      const contract = await getUniswapV3Router();
+      poolOrder._amountA = web3.utils.toWei("1");
+      poolOrder._amountB = await contract.methods.getTokenOut(poolInfo.poolsAddress, poolInfo.tokenB, web3.utils.toWei("1")).call();
+      setPoolOrder({ ...poolOrder });
       console.log(poolOrder);
-      const contract= await getUniswapV3Router();
-      poolOrder._amountA=web3.utils.toWei("1");
-      poolOrder._amountB=await contract.methods.getToken(poolInfo.poolsAddress,poolInfo.tokenB,web3.utils.toWei("1")).call();
-      setPoolOrder({...poolOrder});
-      console.log(poolOrder);
+      console.log(poolInfo);
     }
 
+   function fromWei(val: any) {
+      return web3.utils.fromWei(val);
+   }
 
-    async function approveErc20(address:any){
-        const contract= await getErc20Contract(address);
-        contract.methods.approve(poolInfo.poolsAddress,web3.utils.toWei("999999999")).send({
+
+   async function approveErc20(_address: any) {
+      const contract = await getErc20Contract(_address);
+      contract.methods.approve(poolInfo.poolsAddress, web3.utils.toWei("999999999")).send({
          from: address
-       }).on('error', (error: any) =>{
+      }).on('error', (error: any) => {
          message.error(error);
          getErc20Approve();
-       }).on('transactionHash', (txHash: any) => {
+      }).on('transactionHash', (txHash: any) => {
          console.warn("transactionHash", txHash)
-       }).on('receipt', (receipt: any) => {
-           message.success("Success");
-           getErc20Approve();
-       })
-    }
+      }).on('receipt', (receipt: any) => {
+         message.success("Success");
+         getErc20Approve();
+      })
+   }
 
-    async function approve721(){
-      const contract= await getErc721Contract(poolInfo.nft_address);
-      contract.methods.setApprovalForAll(poolInfo.fractionNFTAddress,true).send({
-       from: address
-     }).on('error', (error: any) =>{
-       message.error(error);
-       getErc721Approve();
-     }).on('transactionHash', (txHash: any) => {
-       console.warn("transactionHash", txHash)
-     }).on('receipt', (receipt: any) => {
+   async function approve721() {
+      const contract = await getErc721Contract(poolInfo.nft_address);
+      contract.methods.setApprovalForAll(poolInfo.fractionNFTAddress, true).send({
+         from: address
+      }).on('error', (error: any) => {
+         message.error(error);
+         getErc721Approve();
+      }).on('transactionHash', (txHash: any) => {
+         console.warn("transactionHash", txHash)
+      }).on('receipt', (receipt: any) => {
          message.success("Success");
          getErc721Approve();
-     })
-  }
+      })
+   }
 
-   
-   async function addPool(){
+
+   async function addPool() {
+      if (!poolOrder.tokenId) {
+         message.error("Please select the NFT to be added");
+         return;
+      }
+      debugger;
       const uniswapV3Router = await getUniswapV3Router();
-      await uniswapV3Router.methods.addPool721(nftaddress,getTokenB(),12,web3.utils.toWei("1"),web3.utils.toWei("80")).send({
+      await uniswapV3Router.methods.addPool721(nftaddress, getTokenB(), 12, web3.utils.toWei("1"), web3.utils.toWei("80")).send({
          from: address
-       }).on('error', (error: any) =>{
+      }).on('error', (error: any) => {
          message.error(error);
          initSwap();
-       }).on('transactionHash', (txHash: any) => {
+      }).on('transactionHash', (txHash: any) => {
          console.warn("transactionHash", txHash)
-       }).on('receipt', (receipt: any) => {
-           message.success("Success");
-           initSwap();
-       })
-    }
+      }).on('receipt', (receipt: any) => {
+         message.success("Success");
+         initSwap();
+      })
+   }
 
-    async function createPool(){
+   async function createPool() {
       const uniswapV3Router = await getUniswapV3Router();
-       let  scale =web3.utils.toWei("80");
-      await uniswapV3Router.methods.createPool(nftaddress,0,getTokenB(),scale).send({
+      let scale = web3.utils.toWei("80");
+      await uniswapV3Router.methods.createPool(nftaddress, 0, getTokenB(), scale).send({
          from: address
-       }).on('error', (error: any) =>{
+      }).on('error', (error: any) => {
          message.error(error);
          initSwap();
-       }).on('transactionHash', (txHash: any) => {
+      }).on('transactionHash', (txHash: any) => {
          console.warn("transactionHash", txHash)
-       }).on('receipt', (receipt: any) => {
-           message.success("Success");
-           initSwap();
-       })
-    }
+      }).on('receipt', (receipt: any) => {
+         message.success("Success");
+         initSwap();
+      })
+   }
 
-
-
-
-
-    const onChange = (e:any) => {
-      if(!poolOrder.nftApprove){
+   const onChange = (e: any) => {
+      if (!poolOrder.nftApprove) {
          approve721();
       }
-      poolOrder.tokenId= e.target.value;
-      setPoolOrder({...poolOrder});
+      poolOrder.tokenId = e.target.value;
+      setPoolOrder({ ...poolOrder });
       initOrder();
+   }
+
+   const text = poolInfo.poolsAddress ? <div onClick={addPool} >ADD POOL</div> : <div onClick={createPool}  >CREATE POOL</div>
+
+   const text1 = poolOrder.tokenAApprove == '0' ? <div onClick={e => { approveErc20(poolInfo.tokenA) }}>Approve</div>
+      : poolOrder.tokenBApprove == '0' ? <div onClick={e => { approveErc20(poolInfo.tokenB) }}>Approve</div> : text
 
 
-    }
 
+   const [minPrice, setMinPrice] = useState(0);
+   const [maxPrice, setMaxPrice] = useState(0);
 
-   const text = poolInfo.poolsAddress ? <div onClick={addPool} >ADD POOL</div> : <div  onClick={createPool}  >CREATE POOL</div> 
-	
+   function initPrice() {
+      setMinPrice(78);
+      setMaxPrice(81);
+   }
+
+   function changePrice(type: any, orientation: any) {
+      if (type == 'min') {
+         if (orientation == 'add') {
+            let price = minPrice + 1;
+            if (maxPrice > price) {
+               setMinPrice(price);
+            }
+         } else {
+            setMinPrice(minPrice - 1);
+         }
+      } else {
+         if (orientation == 'add') {
+            setMaxPrice(maxPrice + 1);
+         } else {
+            let price = maxPrice - 1;
+            if (minPrice < price) {
+               setMaxPrice(price);
+            }
+         }
+      }
+   }
+
    useEffect(() => {
-      if(address){
+      if (address) {
          getNftsForOwner();
          initSwap();
       }
-	}, [address, dispatch]);
+   }, [address, dispatch]);
 
 
 
@@ -238,24 +276,24 @@ export function CPools_add() {
                      <div>ETH</div>
                      <DownOutlined />
                   </div>
-                  <div  onClick={e=>{setModalOpen(true)}} className="pools-add-token-name" >
-                      Azuki
+                  <div onClick={e => { setModalOpen(true) }} className="pools-add-token-name" >
+                     Azuki
                   </div>
                </div>
                <div className="pools-add-token-empty" >
-                  <div  style={{width:"100%"}} >0.5%   swap fee</div> 
-                  <div>5% royalty fee</div> 
+                  <div style={{ width: "100%" }} >0.5%   swap fee</div>
+                  <div>5% royalty fee</div>
                </div>
                <div className="pools-add-deposit" >Deposit Amounts</div>
                <div className="pools-add-deposit-amount" >
-                  <div>0.00</div>
+                  <div>{poolOrder._amountB ? fromWei(poolOrder._amountB) : 0.00}</div>
                   <div className="flex-all-center" >
                      <img className="pools-add-setting-img" src={require("../../assets/img/eth.png")} alt="" />
                      <div className="pools-add-token-erc20"   >ETH</div>
                   </div>
                </div>
                <div className="pools-add-deposit-nft pools-add-deposit-amount" >
-                  <div>0.00</div>
+                  <div>{poolOrder._amountA ? fromWei(poolOrder._amountA) : 0.00}</div>
                   <div className="pools-add-deposit-nft-name " >
                      Azuki
                   </div>
@@ -268,9 +306,9 @@ export function CPools_add() {
                   <div className="pools-add-section">
                      <div className="flex-center-width-full" >Min Price</div>
                      <div className="flex-center-width-full-between pools-add-padding" >
-                        <div>-</div>
-                        <div>0</div>
-                        <div>+</div>
+                        <div onClick={() => { changePrice('min', 'reduce') }}>-</div>
+                        <div>{minPrice}</div>
+                        <div onClick={() => { changePrice('min', 'add') }}>+</div>
                      </div>
 
                      <div className="flex-center-width-full" >
@@ -280,9 +318,9 @@ export function CPools_add() {
                   <div className="pools-add-section">
                      <div className="flex-center-width-full" >Max Price</div>
                      <div className="flex-center-width-full-between pools-add-padding" >
-                        <div>-</div>
-                        <div>0</div>
-                        <div>+</div>
+                        <div onClick={() => { changePrice('max', 'reduce') }}>-</div>
+                        <div>{maxPrice}</div>
+                        <div onClick={() => { changePrice('max', 'add') }}>+</div>
                      </div>
 
                      <div className="flex-center-width-full" >
@@ -290,8 +328,8 @@ export function CPools_add() {
                      </div>
                   </div>
                </div>
-               <div className="pools-add-full-range"  > Full Range</div>
-               <div className="pools-add-but"  >{text}</div>
+               <div className="pools-add-full-range" onClick={() => { initPrice() }}> Full Range</div>
+               <div className="pools-add-but"  >{text1}</div>
             </div>
          </div>
 
@@ -305,7 +343,7 @@ export function CPools_add() {
             <div className="pools-add-your-nfts-text" >
                <LeftOutlined />
                <div className="pools-add-your-nfts-text-Your"  >Your NFTs</div>
-             
+
             </div>
 
             <div className="flex-center-width-full" >
@@ -316,23 +354,23 @@ export function CPools_add() {
 
 
             <div className="pools-add-your-nft-list" >
-                <Radio.Group  onChange={e=>onChange(e)}  style={{width:"100%"}} >
-               { myNfts.length > 0 ?
-                  myNfts.map((item, i) => {
-                     return <div key={i} className="pools-add-your-nft-list-item" >
-                        <div className="pools-add-your-nft-list-item-img" style={{backgroundImage:"url("+item.tokenUrl+")"}} >
-                           <Radio value={item.tokenId}></Radio>
-                        </div>
+               <Radio.Group onChange={e => onChange(e)} style={{ width: "100%" }} >
+                  {myNfts.length > 0 ?
+                     myNfts.map((item, i) => {
+                        return <div key={i} className="pools-add-your-nft-list-item" >
+                           <div className="pools-add-your-nft-list-item-img" style={{ backgroundImage: "url(" + item.tokenUrl + ")" }} >
+                              <Radio value={item.tokenId}></Radio>
+                           </div>
 
-                        <div className="pools-add-your-nft-list-item-name" >
-                           <div className="pools-add-your-nft-list-item-name-text">{item.name}</div>
-                           <div className="pools-add-your-nft-list-item-id-text" >name #{item.tokenId}</div>
+                           <div className="pools-add-your-nft-list-item-name" >
+                              <div className="pools-add-your-nft-list-item-name-text">{item.name}</div>
+                              <div className="pools-add-your-nft-list-item-id-text" >name #{item.tokenId}</div>
+                           </div>
                         </div>
-                     </div>
-                  })
-                  :
-                  <div></div>
-               }
+                     })
+                     :
+                     <div></div>
+                  }
                </Radio.Group>
             </div>
 
@@ -349,7 +387,7 @@ export function CPools_add() {
 
 
       {/* <Modal   style={{backgroundColor:"#141414",color:"wheat"}}  bodyStyle={{backgroundColor:"#141414"}}  open={isModalOpen} footer={null} onCancel={()=>setModalOpen(false)}  >
-             
+
       </Modal> */}
 
 
