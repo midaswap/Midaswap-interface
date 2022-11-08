db997110edbd6f603cb87acbfd39353364355769
scott585 <ling5201314a>
2022-11-09 01:12:49 +0800

    update

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -11,14 +11,13 @@ export function Footer() {
 	const Web = <div id="footer"  mode="web"  >
 		<div  className="footer" >
 			<div className="footer-img"  >
-				<img   onClick={Common.toTwitter}  className="img-tg img-marg" src={require("../../assets/img/tg.png")} />
-				<img   onClick={Common.toDiscord}   className="img-tg img-marg" src={require("../../assets/img/discode.png")} />
-				<img   onClick={Common.toGithup} className="img-tg img-marg" src={require("../../assets/img/email.png")} />
-				<div  className="footer_Docs" >Docs</div>
+				<img   onClick={Common.toTwitter}  className="img-tg img-marg" src={require("../../assets/img/f_twitter.png")} />
+				<img   onClick={Common.toDiscord}   className="img-tg img-marg" src={require("../../assets/img/f_discode.png")} />
+				<img   onClick={Common.toGithup} className="img-tg img-marg" src={require("../../assets/img/f_githup.png")} />
+				<div   onClick={Common.toDocs}  className="footer_Docs" >Docs</div>
 			</div>
 		</div>
 	</div>
-
 	// @ts-ignore
 	const Mobile = <div id="footer" mode="mobile"></div>
 	return Common.isMobile ? Mobile : Web
