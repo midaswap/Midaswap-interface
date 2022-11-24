6dd3504002cb4f7ecea61c5d1d10924538b6c6b5
scott585 <sccottfast@gmail.com>
2022-11-25 02:20:54 +0800

    build

---
+++ build/static/js/31.e0561e52.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[31],{1605:function(s,e){s.exports={messages:{}}}}]);
+//# sourceMappingURL=31.e0561e52.chunk.js.map
---
+++ build/static/js/64.ac78d720.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[64],{1912:function(s,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return t}));var t={messages:{}}}}]);
+//# sourceMappingURL=64.ac78d720.chunk.js.map
